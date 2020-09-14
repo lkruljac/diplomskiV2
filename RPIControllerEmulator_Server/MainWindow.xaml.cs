1d161379f5df79f5076ee8c7ac7d7b2c5f784750
luka <luka97kruljac@gmail.com>
2020-09-14 22:50:37 +0200

    message over network sent

--- RPIControllerEmulator_Server/MainWindow.xaml.cs
+++ RPIControllerEmulator_Server/MainWindow.xaml.cs
@@ -12,7 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DarrenLee.Bluetooth;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
 
 namespace RPIControllerEmulator_Server
 {
@@ -36,11 +41,12 @@ namespace RPIControllerEmulator_Server
             }
             else if (bluetootRadioButton.IsChecked == true)
             {
-                BluetoothConnect();
+                MessageBox.Show("Selected connection is " + conectionType);
+                ConnectBluetooth();
             }
-            else if (networkRadioButton.IsChecked==true)
+            else if (networkRadioButton.IsChecked == true)
             {
-
+                ConnectNetwork();
                 conectionType = "network";
             }
             else
@@ -49,26 +55,65 @@ namespace RPIControllerEmulator_Server
                 return;
             }
 
-            MessageBox.Show("Selected connection is " + conectionType);
+
         }
 
-        private void BluetoothConnect()
+        private void ConnectBluetooth()
         {
-            Bluetooth_Server btServer = new Bluetooth_Server();
 
-            btServer.Start();
-            btServer.IsConnected += BtServer_IsConnected;
-            btServer.DataReceived += BtServer_DataReceived;
         }
-
-        private void BtServer_DataReceived(object sender, BluetoothServerEventArgs e)
+        private void ConnectNetwork()
         {
-            throw new NotImplementedException();
-        }
+            string message = "Hello";
+            string server = "192.168.1.113";
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 54000;
+                TcpClient client = new TcpClient(server, port);
 
-        private void BtServer_IsConnected(object sender, EventArgs e)
-        {
-            
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                MessageBox.Show("Received: " + responseData);
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+
         }
     }
 }
