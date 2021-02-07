9e5195df4877531fa9b378f43bff93ebb0c749cb
lkruljac <luka97kruljac@gmail.com>
2021-02-07 14:15:50 +0100

    Test

--- Diplomski V2.1/SystemForIndirectUseOfInputDevices/Common/BaseViewModel.cs
+++ Diplomski V2.1/SystemForIndirectUseOfInputDevices/Common/BaseViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestDesignerAndExecutor.Installer.Common
+namespace Common
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
--- Diplomski V2.1/SystemForIndirectUseOfInputDevices/Common/DelegateCommand.cs
+++ Diplomski V2.1/SystemForIndirectUseOfInputDevices/Common/DelegateCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace TestDesignerAndExecutor.Installer.Common
+namespace Common
 {
     public class DelegateCommand : ICommand
     {
