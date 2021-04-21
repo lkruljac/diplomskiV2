7fd4832063e7c7399bd0c0f191cfd8d1d62a7f9d
luka <luka97kruljac@gmail.com>
2021-04-21 16:28:39 +0200

    first week

--- Diplomski V2.1/PythonCar/2wheel/2wheel/car.py
+++ Diplomski V2.1/PythonCar/2wheel/2wheel/car.py
@@ -2,11 +2,37 @@ import RPi.GPIO as GPIO
 
 class Car:
 
-    def __init__():
-        
-    def drive:
-        
-    def stop(self):
-        """ Set the duty cycle of both control pins to zero to stop the motor. """
-        self.pwm_forward.ChangeDutyCycle(0)
-        self.pwm_backward.ChangeDutyCycle(0)
+    def __init__(self):
+        global RPWM
+        self.RPWM = 9
+        global RIN1 
+        RIN1 = 10
+        global RIN2 
+        RIN2 = 11
+        global LIN1 
+        LIN1 = 23
+        global LIN2 
+        LIN2 = 24
+        global LPWM
+        LPWM = 25
+
+        GPIO.setup(RPWM, GPIO.OUT)
+        GPIO.setup(RIN1, GPIO.OUT)
+        GPIO.setup(RIN2, GPIO.OUT)
+
+        GPIO.setup(LIN1, GPIO.OUT)
+        GPIO.setup(LIN2, GPIO.OUT)
+        GPIO.setup(LPWM, GPIO.OUT)
+
+
+    def drive(direction, rspeed, lspeed):
+        if direction > 0:
+            GPIO.OUT(RIN1, 0)
+            GPIO.output(RIN2, 1)
+            GPIO.output(self.LIN1, 0)
+            GPIO.output(LIN2, 1)
+        else:
+            GPIO.output(RIN1, 1)
+            GPIO.output(RIN2, 0)
+            GPIO.output(LIN1, 1)
+            GPIO.output(LIN2, 0)
