f59b5d4408543cf9318501ddeb2911b4bb47db62
lkruljac <luka.kruljac@infineon.com>
2021-05-11 18:09:50 +0200

    added enums and started with message generator

---
+++ Diplomski V2.1/SystemForIndirectUseOfInputDevices/DesktopApplication/Model/EDeviceType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public enum EDeviceType
+    {
+        None = 0,
+        Keyboard = 1,
+        Mouse = 2,
+        Joystick = 3 
+    }
+}
---
+++ Diplomski V2.1/SystemForIndirectUseOfInputDevices/DesktopApplication/Services/MessageGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Linearstar.Windows.RawInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class MessageGenerator
+    {
+
+        public static Byte[] GenerateByteArray(RawInputData rawInputData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GenerateString(RawInputData rawInputData)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
