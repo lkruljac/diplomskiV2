9fe62385aa0cc391cef28f78565b0364b1e5b420
luka <luka97kruljac@gmail.com>
2021-05-14 10:31:20 +0200

    added missing files

---
+++ Diplomski V2.1/SystemForIndirectUseOfInputDevices/DesktopApplication/Services/Devices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+using Model;
+using System.Collections.ObjectModel;
+
+namespace Services
+{
+    public static class Devices
+    {
+        
+        public static ObservableCollection<DeviceModel> GetAllDevices()
+        {
+            ObservableCollection<DeviceModel> devices = new ObservableCollection<DeviceModel>();
+
+            //{
+            //    new DeviceModel(null, null, "Keyboard"),
+            //    new DeviceModel(null, null, "Mouse"),
+            //    new DeviceModel(null, null, "Joystick")
+            //};
+
+            foreach(DeviceModel item in RawInput.RawInputWrapper.GetAllDevices())
+            {
+                devices.Add(item);
+            }
+
+            return devices;
+        }       
+    }
+}
